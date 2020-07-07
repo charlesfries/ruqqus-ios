45651a885a8208c4e2736c03d718bb545155f597
Charles Fries <charliefries@icloud.com>
2020-07-06 20:56:13 -0700

    Changes

--- Ruqqus/Views/RowViews.swift
+++ Ruqqus/Views/RowViews.swift
@@ -12,8 +12,8 @@ struct PostRow: View {
     var isFeed: Bool
     var post: Post
     
-    @State private var action: Int? = 0
-    @State private var action2: Int? = 0
+    @State private var toGuildAction: Int? = 0
+    @State private var toUserAction: Int? = 0
 
     var body: some View {
         NavigationLink(destination: PostView(post: post)) {
@@ -27,17 +27,17 @@ struct PostRow: View {
                     Text(post.title)
                     HStack {
                         if isFeed {
-                            Button(action: { self.action = 1 }) {
+                            Button(action: { self.toGuildAction = 1 }) {
                                 Text("+\(post.guild)")
                                     .foregroundColor(Color("Purple"))
                             }.buttonStyle(PlainButtonStyle())
-                            NavigationLink(destination: GuildView(name: post.guild), tag: 1, selection: $action) {}
+                            NavigationLink(destination: GuildView(name: post.guild), tag: 1, selection: $toGuildAction) {}
                         } else {
-                            Button(action: { self.action = 1 }) {
+                            Button(action: { self.toUserAction = 1 }) {
                                 Text(post.user)
                                     .foregroundColor(Color("Purple"))
-                            }
-                            NavigationLink(destination: UserView(username: post.user), tag: 2, selection: $action2) {}
+                            }.buttonStyle(PlainButtonStyle())
+                            NavigationLink(destination: UserView(username: post.user), tag: 2, selection: $toUserAction) {}
                         }
                         Image(systemName: "arrow.up")
                         Text(String(post.score))
@@ -58,6 +58,7 @@ struct PostRow: View {
 
 struct CommentRow: View {
     var comment: Comment
+    var isMini: Bool = true
     @State private var action: Int? = 0
     
     var body: some View {
@@ -72,7 +73,9 @@ struct CommentRow: View {
                 Spacer()
                 Text(comment.createdAt).foregroundColor(Color("Grey"))
             }
-            Text(comment.content)
+            if !isMini {
+                Text(comment.content)
+            }
             
             NavigationLink(destination: UserView(username: comment.user), tag: 1, selection: $action) {
                 EmptyView()
