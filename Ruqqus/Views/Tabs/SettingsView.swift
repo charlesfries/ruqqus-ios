1847d28ce21813261c6a578255711b50b552bdd7
Charles Fries <charliefries@icloud.com>
2020-07-06 21:26:48 -0700

    Update SettingsView

--- Ruqqus/Views/Tabs/SettingsView.swift
+++ Ruqqus/Views/Tabs/SettingsView.swift
@@ -12,15 +12,54 @@ struct SettingsView: View {
     var body: some View {
         NavigationView {
             List {
-                Text("General")
-                Text("Appearance")
-                Text("Notifications")
-                Text("Theme")
-                Text("Face ID & Passcode")
-                Text("Filters & Blocks")
+                NavigationLink(destination: SettingsGeneralView()) {
+                    Text("General")
+                }
+                NavigationLink(destination: SettingsAppearanceView()) {
+                    Text("Appearance")
+                }
+                NavigationLink(destination: SettingsNotificationsView()) {
+                    Text("Notifications")
+                }
+                NavigationLink(destination: Text("Theme")) {
+                    Text("Theme")
+                }
+                NavigationLink(destination: Text("Face ID & Passcode")) {
+                    Text("Face ID & Passcode")
+                }
+                NavigationLink(destination: Text("Filters & Blocks")) {
+                    Text("Filters & Blocks")
+                }
             }.listStyle(GroupedListStyle())
             
             .navigationBarTitle("Settings", displayMode: .inline)
         }
+    }
+}
+
+struct SettingsGeneralView: View {
+    var body: some View {
+        Text("General")
+        .navigationBarTitle("General", displayMode: .inline)
+    }
+}
+
+struct SettingsAppearanceView: View {
+    var body: some View {
+        Text("Appearance")
+        .navigationBarTitle("Appearance", displayMode: .inline)
+    }
+}
+
+struct SettingsNotificationsView: View {
+    var body: some View {
+        Text("Notifications")
+        .navigationBarTitle("Notifications", displayMode: .inline)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
     }
 }
